411ac3f71cc337b50b8f4ffb2f1fa6fc392ac8c8
Wyatt <wkirschn@uoguelph.ca>
2020-12-07 02:34:53 -0500

    Added Delete Functionality, need to make this easier for passport validation

--- routes/items.js
+++ routes/items.js
@@ -21,7 +21,30 @@ router.get('/', function(req, res, next) {
         }
         else {
             res.render('items/index', {
-                title: 'Item Directory',
+                title: 'Brief Item Directory',
+                items:items,
+                layout: 'layout.hbs'
+            })
+        }
+    })
+
+
+
+
+})
+/* GET item database view. */
+router.get('/all', function(req, res, next) {
+
+    //1. Fetch the Items to place onto the view
+    Item.find((err, items) =>
+    {
+        if(err) {
+            console.log(err)
+            res.end(err)
+        }
+        else {
+            res.render('items/all', {
+                title: 'All Items Defined',
                 items:items
             })
         }
@@ -31,6 +54,8 @@ router.get('/', function(req, res, next) {
 
 
 })
+
+
 
 // GET items add view
 
@@ -44,11 +69,11 @@ router.post('/add', (req,res, next) =>
     //use mongooseDB
     Item.create({
         name: req.body.name,
-        //   description: req.body.description,
+        description: req.body.description,
         image: req.body.image,
-        // resin_id: req.body.resin_id,
-        //  disposal_method: req.body.disposal_method,
-        // eco_comment: req.body.eco_comment,
+        resin_id: req.body.resin_id,
+        disposal_method: req.body.disposal_method,
+        eco_comment: req.body.eco_comment,
         ecoScore: req.body.ecoScore
 
     }, (err, task) => {
@@ -62,6 +87,27 @@ router.post('/add', (req,res, next) =>
     })
 })
 
+// GET items/delete/ - colon in the path represents a URL parameter
+
+router.get('/delete/:_id', (req,res, next) => {
+    //Store the selected id in a local variable
+    var _id = req.params._id;
+    // Use Mongoose to delete the selected document from the DB
+    Item.remove({_id: _id}, (err) =>
+    {
+        if(err) {
+            console.log(err)
+            res.end(err)
+        }
+        else {
+            res.redirect('/items')
+        }
+
+
+
+    })
+
+})
 
 
 module.exports = router;
